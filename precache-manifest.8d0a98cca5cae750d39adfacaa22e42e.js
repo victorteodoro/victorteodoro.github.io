d6942c1f9e35ec215b7ab05a6394480f83d2100c
Victor Teodoro <victorjst@hotmail.com>
2019-02-27 19:29:29 -0300

    Updates

---
+++ precache-manifest.8d0a98cca5cae750d39adfacaa22e42e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3d8aa76646bd6a9b71cc",
+    "url": "/static/js/main.3d8aa766.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "3d8aa76646bd6a9b71cc",
+    "url": "/static/css/main.0404602b.chunk.css"
+  },
+  {
+    "revision": "0fd701028341f3c65a11ce2257770e4a",
+    "url": "/index.html"
+  }
+];
